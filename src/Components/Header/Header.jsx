37e2e54b5bcbdd4c487fdb7c18a09d319fbe2b50
uthso297 <uthso119464@gmail.com>
2024-10-22 22:55:40 +0600

    completed hero

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,14 +2,14 @@ import logo from '../../assets/images/logo.webp'
 import { IoIosContact } from "react-icons/io";
 const Header = () => {
     return (
-        <div className='flex items-center justify-between max-w-7xl mx-auto md:p-4 p-2 bg-red-500'>
+        <div className='flex items-center justify-between max-w-7xl mx-auto md:p-4 p-2'>
             <div className='flex md:gap-2 gap-1'>
                 <img src={logo} alt="" />
                 <h2 className='md:text-3xl text-xl font-bold'>Peddy</h2>
             </div>
 
             <div >
-            <ul className='flex gap-7'>
+            <ul className='flex gap-7 md:mr-[110px] mr-0'>
                     <li>Home</li>
                     <li>Shop</li>
                     <li>Contact</li>
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,15 +1,23 @@
 import { FaHeart } from "react-icons/fa";
+import heroImage from '../../assets/images/pet.webp'
 const Hero = () => {
     return (
-        <div>
+        <div className="max-w-7xl mx-auto space-y-5 my-3">
 
-            <div className="flex justify-center bg-blue-500 max-w-7xl mx-auto">
+            <div className="flex justify-center  gap-1">
                 <p className="font-bold opacity-50">Bringing Families Together</p>
                 <span className="text-2xl"><FaHeart /></span>
             </div>
-
-            
-
+            <div className="space-y-3">
+                <h1 className="text-5xl text-center">Your Path to Adopotion <br /> Starts Here</h1>
+                <p className="text-center">Your journey to finding the perfect furry friend begins with us. Explore our resources and adopt a pet today!</p>
+                <div className="text-center">
+                    <button className="btn bg-[#127982] font-bold rounded-lg px-3 py-2">View More</button>
+                </div>
+            </div>
+            <div>
+                <img className="mx-auto" src={heroImage} alt="" />
+            </div>
         </div>
     );
 };
