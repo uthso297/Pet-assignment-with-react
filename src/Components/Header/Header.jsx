4e55a2270826a68e3e7248c135d8d33aa6b1dbad
uthso297 <uthso119464@gmail.com>
2024-10-22 22:33:06 +0600

    completed navbar

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,22 @@
 import logo from '../../assets/images/logo.webp'
-
+import { IoIosContact } from "react-icons/io";
 const Header = () => {
     return (
-        <div>
-            <div>
+        <div className='flex items-center justify-between max-w-7xl mx-auto md:p-4 p-2 bg-red-500'>
+            <div className='flex md:gap-2 gap-1'>
                 <img src={logo} alt="" />
+                <h2 className='md:text-3xl text-xl font-bold'>Peddy</h2>
+            </div>
+
+            <div >
+            <ul className='flex gap-7'>
+                    <li>Home</li>
+                    <li>Shop</li>
+                    <li>Contact</li>
+                </ul>
+            </div>
+            <div className='md:text-3xl text-lg'>
+            <IoIosContact />
             </div>
         </div>
     );
---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import { FaHeart } from "react-icons/fa";
+const Hero = () => {
+    return (
+        <div>
+
+            <div className="flex justify-center bg-blue-500 max-w-7xl mx-auto">
+                <p className="font-bold opacity-50">Bringing Families Together</p>
+                <span className="text-2xl"><FaHeart /></span>
+            </div>
+
+            
+
+        </div>
+    );
+};
+
+export default Hero;
