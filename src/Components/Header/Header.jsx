1487d62cc9192b7d55c1e51e0b5443876075ea7f
uthso297 <uthso119464@gmail.com>
2024-10-22 21:31:15 +0600

    created react basic

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import logo from '../../assets/images/logo.webp'
+
+const Header = () => {
+    return (
+        <div>
+            <div>
+                <img src={logo} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default Header;
